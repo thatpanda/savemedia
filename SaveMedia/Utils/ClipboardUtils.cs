296c09fcc9f6684b975db1d7836ebb7c3ab76526
thatpanda <thatpanda@users.noreply.github.com>
2012-06-20 12:45:43 +0800

    ~ retrieve clipboard text in UnicodeText format

--- SaveMedia/Utils/ClipboardUtils.cs
+++ SaveMedia/Utils/ClipboardUtils.cs
@@ -6,31 +6,15 @@ namespace Utility
     {
         public static String ReadUrl()
         {
-            String theClipboardString = ReadText();
-            String theUrl = String.Empty;
-
-            if( !String.IsNullOrEmpty( theClipboardString ) )
+            if( System.Windows.Forms.Clipboard.ContainsText() )
             {
                 Uri theUri;
-                bool isValidUrl = Uri.TryCreate( theClipboardString, UriKind.Absolute, out theUri );
-
-                if( isValidUrl )
+                if( Uri.TryCreate( System.Windows.Forms.Clipboard.GetText(), UriKind.Absolute, out theUri ) )
                 {
-                    theUrl = theUri.OriginalString;
+                    return theUri.OriginalString;
                 }
             }
 
-            return theUrl;
-        }
-
-        public static String ReadText()
-        {
-            System.Windows.Forms.IDataObject theData = System.Windows.Forms.Clipboard.GetDataObject();
-
-            if( theData.GetDataPresent( System.Windows.Forms.DataFormats.Text ) )
-            {
-                return (String)theData.GetData( System.Windows.Forms.DataFormats.Text );
-            }
             return String.Empty;
         }
     }
