2134c208588dc19dd0ec2743eb0d09edc25b72af
thatpanda <thatpanda@users.noreply.github.com>
2013-01-14 11:20:43 +0800

    update UserAgent

--- SaveMedia/Program.cs
+++ SaveMedia/Program.cs
@@ -9,11 +9,11 @@ namespace SaveMedia
         public const String Description     = "";           // Comments
         public const String Product         = "SaveMedia";  // Product Name
         public const String Company         = "";
-        public const String Copyright       = "© 2012 Eric Choy";
+        public const String Copyright       = "© 2013 Eric Choy";
         public const String AssemblyVersion = "1.0.0.0";
         public const String FileVersion     = "1.7.8.0";
         public const String TitleVersion    = "1.7.8";
-        public const String UserAgent       = "Mozilla/5.0 (Windows NT 6.1; rv:15.0) Gecko/20100101 Firefox/15.0.1";
+        public const String UserAgent       = "Mozilla/5.0 (Windows NT 6.1; rv:18.0) Gecko/20100101 Firefox/18.0";
 
         /// <summary>
         /// The main entry point for the application.
