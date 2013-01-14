f84cc16407f9a3e8d2db9666a863f30cb693da0c
thatpanda <thatpanda@users.noreply.github.com>
2013-01-14 11:14:56 +0800

    set accept button

--- SaveMedia/UI/MainForm.Designer.cs
+++ SaveMedia/UI/MainForm.Designer.cs
@@ -355,6 +355,7 @@
             // 
             // MainForm
             // 
+            this.AcceptButton = this.mDownloadButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
