b662dd1ac120bd96c4c57cee5f7b4a1577433e53
thatpanda <thatpanda@users.noreply.github.com>
2015-05-26 00:17:13 -0700

    Bump version number

--- metadata.py
+++ metadata.py
@@ -1,7 +1,7 @@
 author = "Eric Choy"
 name = "SaveMedia"
-version = "3.0.0.0"
-display_version = "3.0a"
+version = "3.0.1.0"
+display_version = "3.0.1a"
 title = "{0} {1}".format(name, display_version)
 description = "Download and convert videos"
 icon = "savemedia.ico"
